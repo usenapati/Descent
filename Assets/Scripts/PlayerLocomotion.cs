963ab8a0022539e81ec4090518eecccf5c4550cc
Udayan Senapati <udayansenapati@gmail.com>
2022-06-07 01:21:57 -0400

    Jumping

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -24,13 +24,18 @@ public class PlayerLocomotion : MonoBehaviour
     [Header("Movement Flags")]
     public bool isSprinting;
     public bool isGrounded;
+    public bool isJumping;
     public bool isWallRunning;
 
-    [Header("Movement Speed")]
+    [Header("Movement Speeds")]
     public float walkingSpeed = 1.5f;
     public float runningSpeed = 5;
     public float sprintingSpeed = 7;
     public float rotationSpeed = 15;
+
+    [Header("Jump Speeds")]
+    public float jumpHeight = 3;
+    public float gravityIntensity = -15;
 
     private void Awake()
     {
@@ -53,6 +58,9 @@ public class PlayerLocomotion : MonoBehaviour
 
     private void HandleMovement()
     {
+        if (isJumping)
+            return;
+
         moveDirection = cameraObject.forward * inputManager.verticalInput;
         moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
         moveDirection.Normalize();
@@ -80,6 +88,9 @@ public class PlayerLocomotion : MonoBehaviour
 
     private void HandleRotation()
     {
+        if (isJumping)
+            return;
+
         Vector3 targetDirection = Vector3.zero;
 
         targetDirection = cameraObject.forward * inputManager.verticalInput;
@@ -100,9 +111,10 @@ public class PlayerLocomotion : MonoBehaviour
     {
         RaycastHit hit;
         Vector3 rayCastOrigin = transform.position;
+        Vector3 targetPosition;
         rayCastOrigin.y += rayCastHeightOffset;
-
-        if (!isGrounded)
+        targetPosition = transform.position;
+        if (!isGrounded && !isJumping)
         {
             if (!playerManager.isInteracting)
             {
@@ -110,7 +122,12 @@ public class PlayerLocomotion : MonoBehaviour
             }
 
             inAirTimer += Time.deltaTime;
-            playerRigidBody.AddForce(transform.forward * leapingVelocity);
+
+            if (inputManager.moveAmount >= 0.5f)
+            {
+                playerRigidBody.AddForce(transform.forward * leapingVelocity);
+            }
+            
             playerRigidBody.AddForce(-Vector3.up * fallingVelocity * inAirTimer);
         }
 
@@ -121,12 +138,41 @@ public class PlayerLocomotion : MonoBehaviour
                 animatorManager.PlayTargetAnimation("Landing", true);
             }
 
+            Vector3 rayCastHitPoint = hit.point;
+            targetPosition.y = rayCastHitPoint.y;
             inAirTimer = 0;
             isGrounded = true;
         }
         else
         {
             isGrounded = false;
+        }
+
+        if (isGrounded && !isJumping)
+        {
+            if (playerManager.isInteracting || inputManager.moveAmount > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime / 0.1f);
+            } 
+            else
+            {
+                transform.position = targetPosition;
+            }      
+        }
+    }
+
+    public void HandleJumping()
+    {
+        // If Wall running + Jump away
+        if (isGrounded)
+        {
+            animatorManager.animator.SetBool("IsJumping", true);
+            animatorManager.PlayTargetAnimation("Jumping", false);
+
+            float jumpingVelocity = Mathf.Sqrt(-2 * gravityIntensity * jumpHeight);
+            Vector3 playerVelocity = moveDirection;
+            playerVelocity.y = jumpingVelocity;
+            playerRigidBody.velocity = playerVelocity;
         }
     }
 }
