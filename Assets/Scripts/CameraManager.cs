0bab00817eb5359c0273b04a9ad72a23c250c92f
Udayan Senapati <udayansenapati@gmail.com>
2022-06-10 02:29:05 -0400

    Merged AI with Aim Camera

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -17,27 +17,6 @@ public class CameraManager : MonoBehaviour
     
     public CinemachineVirtualCamera currentCamera; // The transform of the aim camera
 
-    private float defaultPosition;
-    private Vector3 cameraFollowVelocity = Vector3.zero;
-    private Vector3 cameraVectorPosition;
-
-    [Header("Camera Collision Values")]
-    public LayerMask collisionLayers;
-    public float cameraCollisionOffset = 0.2f; // How much the camera will jump off of objects its colliding with
-    public float minimumCollisionOffset = 0.2f;
-    public float cameraCollisionRadius = 0.2f;
-
-    [Header("Camera Speed Values")]
-    public float cameraFollowSpeed = 0.2f;
-    public float cameraLookSpeed = 2; // sensX
-    public float cameraPivotSpeed = 2; // sensY
-
-    [Header("Camera Angle Values")]
-    public float lookAngle; // Camera looking up and down
-    public float pivotAngle; // Camera looking left and right
-    public float minPivotAngle = -35;
-    public float maxPivotAngle = 35;
-
     public bool isAiming;
     public bool onController;
 
@@ -59,7 +38,6 @@ public class CameraManager : MonoBehaviour
         inputManager = FindObjectOfType<InputManager>();
         targetTransform = FindObjectOfType<PlayerManager>().transform;
         currentCamera = cameraFollow;
-        defaultPosition = currentCamera.transform.localPosition.z;
         isAiming = false;
     }
 
@@ -68,11 +46,11 @@ public class CameraManager : MonoBehaviour
         UpdateCurrentCamera();
         UpdateCameraSpeed();
         UpdateCameraSensitivity();
-        RotateCamera();
     }
 
     private void UpdateCurrentCamera()
     {
+        isAiming = inputManager.aim_Input;
         if (isAiming)
         {
             currentCamera.Priority = 9;
@@ -112,57 +90,6 @@ public class CameraManager : MonoBehaviour
             cameraFollow.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = mouseYSensitivity;
         }
     }
-
-    //private void FollowTarget()
-    //{
-    //    Vector3 targetPosition = Vector3.SmoothDamp
-    //        (transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
-    //    transform.position = targetPosition;
-    //}
-
-    private void RotateCamera()
-    {
-        //Vector3 rotation;
-        //Quaternion targetRotation;
-        //lookAngle += (inputManager.cameraInputX * cameraLookSpeed);
-        //pivotAngle -= (inputManager.cameraInputY * cameraPivotSpeed);
-        //pivotAngle = Mathf.Clamp(pivotAngle, minPivotAngle, maxPivotAngle);
-
-        //rotation = Vector3.zero;
-        //rotation.y = lookAngle;
-        //targetRotation = Quaternion.Euler(rotation);
-        //transform.rotation = targetRotation;
-
-        //rotation = Vector3.zero;
-        //rotation.x = pivotAngle;
-        //targetRotation = Quaternion.Euler(rotation);
-        //cameraPivotTransform.localRotation = targetRotation;
-
-        
-    }
-
-    //private void HandleCameraCollisions()
-    //{
-    //    float targetPosition = defaultPosition;
-    //    RaycastHit hit;
-    //    Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
-    //    direction.Normalize();
-
-    //    if (Physics.SphereCast
-    //        (cameraPivotTransform.position, cameraCollisionRadius, direction, out hit, Mathf.Abs(targetPosition), collisionLayers))
-    //    {
-    //        float distance = Vector3.Distance(cameraPivotTransform.position, hit.point);
-    //        targetPosition = -(distance - cameraCollisionOffset);
-    //    }
-
-    //    if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
-    //    {
-    //        targetPosition = targetPosition - minimumCollisionOffset;
-    //    }
-
-    //    cameraVectorPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, 0.2f);
-    //    cameraTransform.localPosition = cameraVectorPosition;
-    //}
 
     public void DoFov(float endValue)
     {
